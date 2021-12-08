b470f7e7116366bb8cecd3d3975e2827ba831a58
Claire Wallerick <clairewallerick@Claires-MacBook-Pro-2.local>
2021-12-08 01:06:27 -0600

    vendors

--- startbootstrap-agency-gh-pages/js/index.js
+++ startbootstrap-agency-gh-pages/js/index.js
@@ -590,7 +590,6 @@ function displayVendorProfile(_vendoremail, _vendorfirstname, _vendorlastname, _
     //const allBoothsUrl = "https://localhost:5001/api/vendorbooth";
     const allBoothsUrl = "https://farmersmarketapi1.herokuapp.com/api/vendorbooth";
 
-    const eventID = document.getElementById("eventid").value;
 
     fetch(allBoothsUrl).then(function(response){
         console.log(response);
@@ -605,6 +604,7 @@ function displayVendorProfile(_vendoremail, _vendorfirstname, _vendorlastname, _
 function postBooth(_vendorid, _vendorbusiness, _businesstype, _businessdesc){
     const allBoothsUrl = "https://farmersmarketapi1.herokuapp.com/api/vendorbooth";
     //const allBoothsUrl = "https://localhost:5001/api/vendorbooth";
+    const eventID = document.getElementById("eventid").value;
 
     console.log("made it");
     fetch(allBoothsUrl, {
